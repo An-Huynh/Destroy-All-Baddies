8fc2f2539568ff685913d969db3fc727245ab9a3
An Huynh <anthehuynh@gmail.com>
2017-02-07 23:57:37 -0600

    added documentation for shader interface

--- src/dab/graphic/shader/Shader.java
+++ src/dab/graphic/shader/Shader.java
@@ -1,15 +1,65 @@
 package dab.graphic.shader;
 
+/**
+ * Interface for shader classes will define public methods that
+ * vertex and fragment shaders need to use.
+ * 
+ * @author An Huynh
+ */
+
 public interface Shader {
 	
+	/**
+	 * sets the source of the shader
+	 * to a string containing the GLSL
+	 * source code
+	 * 
+	 * @param sourceCode string containing GLSL code for shader
+	 */
 	public void loadSource(String sourceCode);
+	
+	/**
+	 * compiles the shader
+	 * does not do anything if compile fails
+	 */
 	public void compile();
 	
+	/**
+	 * returns a boolean value indicating if the shader
+	 * compiled successfully or not
+	 * 
+	 * @return boolean representing if compilation
+	 *         was successful
+	 */
 	public boolean compileResult();
+	
+	/**
+	 * returns a string containing the error message
+	 * if compilation was unsuccessful or an
+	 * empty string if compilation was successful
+	 * 
+	 * @return string containing error log
+	 */
 	public String errorLog();
 	
+	/**
+	 * attaches shader to a specified program
+	 * 
+	 * @param program
+	 */
 	public void attach(int program);
+	
+	/**
+	 * detaches the shader from a specified program
+	 * 
+	 * @param program
+	 */
 	public void detach(int program);
 	
+	/**
+	 * deletes the shader
+	 * shader is only deleted after being detached
+	 * from program.
+	 */
 	public void delete();
 }
