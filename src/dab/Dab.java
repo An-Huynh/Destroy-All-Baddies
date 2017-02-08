1cc39a74084cba1ced2953aeb1ada1799cf539ea
An Huynh <anthehuynh@gmail.com>
2017-02-07 22:38:21 -0600

    Start of project structure

---
+++ src/dab/Dab.java
@@ -0,0 +1,15 @@
+package dab;
+
+/**
+ * 
+ * This class is the entry point to the program "Destroying All Baddies"
+ * 
+ * @author An Huynh, Elijah Irvin, Sione Fangaiuiha, Cristopher Huerta
+ *
+ */
+
+public class Dab {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
