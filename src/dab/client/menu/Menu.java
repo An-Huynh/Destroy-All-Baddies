cf7a0f616d1840906f26e3094eeb625187475fca
Pugglesworth <rekilad@live.com>
2017-03-14 16:07:01 -0500

    Implemented menu interface
    Menu interface is now implemented, contains definition for public method
    display.

---
+++ src/dab/client/menu/Menu.java
@@ -0,0 +1,16 @@
+package dab.client.menu
+
+/**
+ * Interface for menu that will simply define the display method
+ * 
+ * @author Eli Irvin
+ */
+
+public interface Menu {
+	
+	/*
+	 * Displays the menu
+	 */
+	public void display();
+	
+}
