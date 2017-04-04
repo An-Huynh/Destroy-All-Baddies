ee348866082ab17b353d66551b84e7def1f48be8
An Huynh <anthehuynh@gmail.com>
2017-04-03 23:58:14 -0500

    Add implementation for drawing zones

---
+++ src/dab/client/graphic/drawable/player/PlayerDrawable.java
@@ -0,0 +1,11 @@
+package dab.client.graphic.drawable.player;
+
+import dab.client.graphic.drawable.Drawable;
+
+public class PlayerDrawable implements Drawable {
+	
+	public void render() {
+		
+	}
+
+}
