a091147752be12b43fcacb78dcd798ff9e4241e9
An Huynh <anthehuynh@gmail.com>
2017-04-04 20:32:22 -0500

    Update to work with new server implementation

--- src/dab/server/GameServer.java
+++ src/dab/server/GameServer.java
@@ -1,25 +1,32 @@
 package dab.server;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 import dab.common.initialization.TileInitializer;
 import dab.common.initialization.ZoneInitializer;
 import dab.server.logic.GameLoop;
 import dab.server.logic.LoopComponentInitializer;
-import dab.server.network.SocketServer;
+import dab.server.network.SocketManager;
+import dab.server.players.PlayerList;
 
 public class GameServer implements Runnable {
-	
-	private int port;
+
+	private SocketManager socketManager;
 	private GameLoop gameLoop;
-	private SocketServer socketServer;
-	
+	private PlayerList playerList;
+	private int port;
 	
 	public GameServer(int port) {
 		this.port = port;
-	}
 		
+		playerList = new PlayerList();
+		socketManager = new SocketManager(playerList);
+		gameLoop = new GameLoop(playerList);
+	}
+	
 	public void preInit() {
+		
 		TileInitializer.preInit();
 		ZoneInitializer.preInit();
 		LoopComponentInitializer.preInit();
@@ -29,40 +36,41 @@ public class GameServer implements Runnable {
 		TileInitializer.init();
 		ZoneInitializer.init();
 		LoopComponentInitializer.init();
-		
-		socketServer = new SocketServer(port);
-		gameLoop = new GameLoop();
 	}
 	
-	public void postInit() {
-		Thread socketServerThread = new Thread(socketServer);
-		socketServerThread.start();
-		
+	public void postInit() throws IOException, InterruptedException {
+		socketManager.start(port);
 		gameLoop.run();
 	}
 	
+	@Override
 	public void run() {
 		preInit();
 		init();
-		postInit();
+		try {
+			postInit();
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void stop() {
 		gameLoop.stop();
+		playerList.stop();
+		socketManager.stop();
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("Starting...");
-		GameServer server = new GameServer(7720);
-		Thread serverThread = new Thread(server);
-		serverThread.start();
+		GameServer gs = new GameServer(7720);
+		Thread gameThread = new Thread(gs);
+		gameThread.start();
 		
 		Scanner in = new Scanner(System.in);
 		while (!in.nextLine().equals("/stop")) {}
-		server.stop();
 		in.close();
-		
-		System.out.println("Stopping...");
+		gs.stop();
 	}
-		
+	
+	
+	
 }
