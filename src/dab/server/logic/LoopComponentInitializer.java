a45d4a6a3828fac351dddeef6e97e6384f60b16b
An Huynh <anthehuynh@gmail.com>
2017-04-04 21:02:14 -0500

    Removed references to HelloTicker

--- src/dab/server/logic/LoopComponentInitializer.java
+++ src/dab/server/logic/LoopComponentInitializer.java
@@ -1,17 +1,11 @@
 package dab.server.logic;
 
-import dab.server.logic.component.HelloTicker;
-
 public class LoopComponentInitializer {
-
-	private static HelloTicker helloTicker;
 	
 	public static void preInit() {
-		helloTicker = new HelloTicker();
 	}
 	
 	public static void init() {
-		GameLoop.registerTickable(helloTicker);
 	}
 	
 }
