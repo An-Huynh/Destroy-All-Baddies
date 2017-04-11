cb14da88a938b879a1a65c6e8fccbeedb5198fa8
An Huynh <anthehuynh@gmail.com>
2017-04-11 18:43:06 -0500

    Update server side movement

--- src/dab/server/logic/component/position/Movement.java
+++ src/dab/server/logic/component/position/Movement.java
@@ -1,115 +1,79 @@
-package dab.server.logic.component;
+package dab.server.logic.component.position;
 
 import java.io.IOException;
 import java.util.Iterator;
 
-import dab.common.entity.attribute.Direction;
 import dab.common.entity.player.Player;
 import dab.common.physics.AABB;
-import dab.common.registry.ZoneRegistry;
-import dab.common.tile.Tile;
-import dab.common.zone.Zone;
+import dab.server.logic.component.Tickable_S;
 import dab.server.network.ClientConnection;
 import dab.server.network.SocketManager;
 import dab.server.players.PlayerList;
 
 public class Movement implements Tickable_S {
-    
-    private PlayerList playerList;
-    private SocketManager socketManager;
-    private AABB boundingBox;
-    
-    public Movement(PlayerList playerList, SocketManager socketManager) {
-        boundingBox = new AABB();
-        this.socketManager = socketManager;
-        this.playerList = playerList;
-    }
-    
-    @Override
-    public void update() {
-        Iterator<String> keyIterator = playerList.getKeyIterator();
-        while (keyIterator.hasNext()) {
-            String playerKey = keyIterator.next();
-            Player player = playerList.getPlayer(playerKey);
-            calculatePossibleLocation(player.getAABB(), player.getDirection());
-            if (checkLocationSafe(this.boundingBox, player.getZone())) {
-                player.getAABB().copy(this.boundingBox);
-            }
-        }
-        
-        // all players have been updated. send to each client
-        keyIterator = playerList.getKeyIterator();
-        while (keyIterator.hasNext()) {
-            String playerKey = keyIterator.next();
-            ClientConnection conn = socketManager.getConnection(playerKey);
-            try {
-                conn.writeObject("update.playerlist");
-                conn.getOut().reset();
-                conn.writeObject(playerList.getPlayerList());
-            } catch (IOException e) {
-                //e.printStackTrace();
-            }
-        }
-    }
-    
-    
-    
-    
-    
-    /*
-    @Override
-    public void update() {
-        Iterator<String> keyIterator = playerList.getKeyIterator();
-        while (keyIterator.hasNext()) {
-            String key = keyIterator.next();
-            Player player = playerList.getPlayer(key);
-            calculatePossibleLocation(player.getAABB(), player.getDirection());
-            if (checkLocationSafe(this.boundingBox, player.getZone())) {
-                player.getAABB().copy(this.boundingBox);
-                try {
-                    synchronized(this.socketManager.getConnection(key).getOut()) {
-                        this.socketManager.getConnection(key).writeObject("update.player.client");
-                        this.socketManager.getConnection(key).getOut().reset();
-                        this.socketManager.getConnection(key).writeObject(player);
-                    }
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    //e.printStackTrace();
-                }
-            }
-        }
-        
-    }
-    */
-    
-    private void calculatePossibleLocation(AABB boundingBox, Direction direction) {
-        this.boundingBox.copy(boundingBox);
-        if (direction == Direction.LEFT) {
-            this.boundingBox.addOffset(-0.1f, 0.0f);
-        } else if (direction == Direction.RIGHT) {
-            this.boundingBox.addOffset(0.1f, 0.0f);
-        }
-    }
-    
-    private boolean checkLocationSafe(AABB bb, String zoneName) {
-        Zone zone = ZoneRegistry.get(zoneName);
-        if (zone != null) {
-            int centerX = (int) Math.floor(bb.getCenter().x);
-            int centerY = (int) Math.floor(bb.getCenter().y);
-            
-            for (int i = centerX-1; i <= centerX+1; ++i) {
-                for (int j = centerY-1; j <= centerY+1; ++j) {
-                    Tile tile = zone.getTile(i, j);
-                    AABB tileAABB = zone.getTileAABB(i, j);
-                    
-                    if (tile != null && tile.isSolid() && tileAABB.testCollision(bb)) {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-        return false;
-    }
+	
+	private PlayerList playerList;
+	private SocketManager socketManager;
+	
+	public Movement(PlayerList playerList, SocketManager socketManager) {
+		this.playerList = playerList;
+		this.socketManager = socketManager;
+	}
+	
+	@Override
+	public void update() {
+		Iterator<Player> players = getPlayerIterator();
+		while (players.hasNext()) {
+			Player player = players.next();
+			updatePlayerPosition(player);
+		}
+		
+		sendUpdateToAllPlayers();
+		
+	}
+	
+	private Iterator<Player> getPlayerIterator() {
+		return playerList.getPlayerList().values().iterator();
+	}
+	
+	private void updatePlayerPosition(Player player) {
+		AABB updatedAABB = MovementCollisionChecker.calculateNextPlayerAABB(player);
+		if (!MovementCollisionChecker.checkPlayerZoneSolidCollision(player)) {
+			setPlayerAABB(player, updatedAABB);
+		}
+	}
+	
+	private Player setPlayerAABB(Player player, AABB aabb) {
+		player.getAABB().copy(aabb);
+		return player;
+	}
+	
+	private void sendUpdateToAllPlayers() {
+		Iterator<Player> players = getPlayerIterator();
+		while (players.hasNext()) {
+			Player player = players.next();
+			sendPlayerPositionToAll(player);
+		}
+	}
+	
+	private void sendPlayerPositionToAll(Player player) {
+		Iterator<Player> clients = getPlayerIterator();
+		while (clients.hasNext()) {
+			ClientConnection conn = getPlayerConnection(clients.next());
+			synchronized (conn.getOut()) {
+				try {
+					conn.writeObject("update.playerlist");
+					conn.getOut().reset();
+					conn.writeObject(playerList.getPlayerList());
+				} catch (IOException e) {
+					// do nothing
+				}
+			}
+		}
+	}
+	
+	private ClientConnection getPlayerConnection(Player player) {
+		return socketManager.getConnection(player.getName());
+	}
     
 }
---
+++ src/dab/server/logic/component/position/MovementCollisionChecker.java
@@ -0,0 +1,158 @@
+package dab.server.logic.component.position;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.joml.Vector2f;
+import org.joml.Vector2i;
+
+import dab.common.entity.attribute.Direction;
+import dab.common.entity.player.Player;
+import dab.common.physics.AABB;
+import dab.common.registry.ZoneRegistry;
+import dab.common.tile.Tile;
+import dab.common.zone.Zone;
+
+public class MovementCollisionChecker {
+
+	public static AABB calculateNextPlayerAABB(Player player) {
+		AABB boundingBox = new AABB().copy(player.getAABB());
+		return updateAABBByDirection(boundingBox, player.getDirection());
+	}
+	
+	public static boolean checkPlayerZoneSolidCollision(Player player) {
+		Zone playerZone = getPlayerZone(player);
+		return checkPlayerCollisionSolidZoneTiles(player, playerZone);
+	}
+	
+	public static boolean checkPlayerZoneCollisionIgnoreSolid(Player player) {
+		Zone playerZone = getPlayerZone(player);
+		return checkPlayerCollisionZoneTilesIgnoreSolid(player, playerZone);
+	}
+	
+	public static boolean checkFuturePlayerZoneSolidCollision(Player player) {
+		AABB possibleAABB = calculateNextPlayerAABB(player);
+		Player tempPlayer =  getPlayerCopy(player);
+		setPlayerAABB(tempPlayer, possibleAABB);
+		return checkPlayerZoneSolidCollision(tempPlayer);
+	}
+	
+	public static boolean checkFuturePlayerZoneCollisionNonSolid(Player player) {
+		AABB possibleAABB = calculateNextPlayerAABB(player);
+		Player tempPlayer =  getPlayerCopy(player);
+		setPlayerAABB(tempPlayer, possibleAABB);
+		return checkPlayerZoneCollisionIgnoreSolid(tempPlayer);
+	}
+	
+	private static AABB updateAABBByDirection(AABB boundingBox, Direction direction) {
+		Vector2f offset = getOffsetByDirection(direction);
+		return addOffsetToAABB(boundingBox, offset);
+	}
+	
+	private static AABB addOffsetToAABB(AABB boundingBox, Vector2f offset) {
+		return boundingBox.addOffset(offset.x, offset.y);
+	}
+	
+	private static final Vector2f getOffsetByDirection(Direction direction) {
+		Vector2f offset = new Vector2f();
+		switch (direction) {
+			case LEFT:
+				System.out.println("left");
+				offset.set(-0.1f, 0.0f); break;
+			case RIGHT:
+				System.out.println("right");
+				offset.set(0.1f, 0.0f); break;
+			default:
+				System.out.println("default");
+				offset.set(0.0f, 0.0f);
+		}
+		return offset;
+	}
+	
+	private static boolean checkPlayerCollisionSolidZoneTiles(Player player, Zone zone) {
+		Iterator<Vector2i> possibleTileLocations = getPossiblePlayerCollisionArea(player)
+							                           .iterator();
+		while (possibleTileLocations.hasNext()) {
+			if (checkCollisionPlayerSolidTileLocation(player, possibleTileLocations.next())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private static ArrayList<Vector2i> getPossiblePlayerCollisionArea(Player player) {
+		ArrayList<Vector2i> possibleCollisionLocations = new ArrayList<Vector2i>();
+		Vector2i playerLocation = getPlayerLocation(player);
+		for (int x = playerLocation.x - 1; x < playerLocation.x + 2; ++x) {
+			for (int y = playerLocation.y - 1; y < playerLocation.y + 2; ++y) {
+				possibleCollisionLocations.add(new Vector2i(x, y));
+			}
+		}
+		return possibleCollisionLocations;
+	}
+	
+	private static Vector2i getPlayerLocation(Player player) {
+		Vector2i playerLocation = new Vector2i();
+		int playerXCoord = convertToInt(player.getCenter().x);
+		int playerYCoord = convertToInt(player.getCenter().y);
+		playerLocation.set(playerXCoord, playerYCoord);
+		return playerLocation;
+	}
+	
+	private static int convertToInt(float value) {
+		return (int) Math.floor(value);
+	}
+	
+	private static boolean checkCollisionPlayerSolidTileLocation(Player player, Vector2i tileLocation) {
+		Zone playerZone = getPlayerZone(player);
+		Tile tile = getTileAtPlayerZone(playerZone, tileLocation);
+		AABB tileAABB = getTileAABBAtPlayerZone(playerZone, tileLocation);
+		return tile != null && tile.isSolid() && checkAABBCollision(player.getAABB(), tileAABB);
+	}
+	
+	private static boolean checkAABBCollision(AABB a1, AABB a2) {
+		return a1.testCollision(a2);
+	}
+	
+	private static Zone getPlayerZone(Player player) {
+		return ZoneRegistry.get(player.getZone());
+	}
+	
+	private static AABB getTileAABBAtPlayerZone(Zone zone, Vector2i tileLocation) {
+		return zone.getTileAABB(tileLocation.x, tileLocation.y);
+	}
+	
+	private static Tile getTileAtPlayerZone(Zone zone, Vector2i tileLocation) {
+		return zone.getTile(tileLocation.x, tileLocation.y);
+	}
+	
+	private static boolean checkPlayerCollisionZoneTilesIgnoreSolid(Player player, Zone zone) {
+		Iterator<Vector2i> possibleTileLocations = getPossiblePlayerCollisionArea(player)
+				                                       .iterator();
+		while (possibleTileLocations.hasNext()) {
+			if (checkCollisionPlayerTileLocationIgnoreSolid(player, possibleTileLocations.next())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private static boolean checkCollisionPlayerTileLocationIgnoreSolid(Player player, Vector2i tileLocation) {
+		Zone playerZone = getPlayerZone(player);
+		Tile tile = getTileAtPlayerZone(playerZone, tileLocation);
+		AABB tileAABB = getTileAABBAtPlayerZone(playerZone, tileLocation);
+		return tile != null && checkAABBCollision(player.getAABB(), tileAABB);
+	}
+	
+	private static Player getPlayerCopy(Player player) {
+		Player playerCopy = new Player();
+		playerCopy.copy(player);
+		return playerCopy;
+	}
+	
+	private static Player setPlayerAABB(Player player, AABB aabb) {
+		player.getAABB().copy(aabb);
+		return player;
+	}
+	
+}
