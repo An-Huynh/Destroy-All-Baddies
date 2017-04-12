ea11060819c5341d1a45cf95cf56532dfcaf456f
An Huynh <anthehuynh@gmail.com>
2017-04-11 23:58:55 -0500

    Fix bug during direction change

--- src/dab/client/event/keyboard/listener/DirectionListener.java
+++ src/dab/client/event/keyboard/listener/DirectionListener.java
@@ -20,20 +20,8 @@ public class DirectionListener implements K_Observer {
     
     @Override
     public void update(int key, int action, int mods) {
-    	if (action == GLFW.GLFW_PRESS) {
-    		handleKeyPress(key);
-    	} else if (action == GLFW.GLFW_RELEASE) {
-    		handleKeyRelease(key);
-    	}
-    }
-    
-    private int getPlayerDirectionKey() {
-    	switch(clientManager.getPlayerList().getMainPlayer().getDirection()) {
-    		case LEFT: 	return GLFW.GLFW_KEY_LEFT;
-    		case UP: 	return GLFW.GLFW_KEY_UP;
-    		case RIGHT:	return GLFW.GLFW_KEY_RIGHT;
-    		case DOWN: 	return GLFW.GLFW_KEY_DOWN;
-    		default: 	return -1;
+    	if (isDirectionKey(key)) {
+    		handleDirection();
     	}
     }
     
@@ -42,29 +30,11 @@ public class DirectionListener implements K_Observer {
     	clientManager.getPlayerList().getMainPlayer().setIsDirectionModified(true);
     }
     
-    private void handleKeyPress(int key) {
-    	if (getPlayerDirectionKey() != key && key == GLFW.GLFW_KEY_LEFT) {
-    		setDirection(Direction.LEFT);
-    	} else if (getPlayerDirectionKey() != key && key == GLFW.GLFW_KEY_UP) {
-    		setDirection(Direction.UP);
-    	} else if (getPlayerDirectionKey() != key && key == GLFW.GLFW_KEY_RIGHT) {
-    		setDirection(Direction.RIGHT);
-    	} else if (getPlayerDirectionKey() != key && key == GLFW.GLFW_KEY_DOWN) {
-    		setDirection(Direction.DOWN);
-    	}
-    }
-    
-    private void handleKeyRelease(int key) {
-    	if (isDirectionKey(key)) {
-    		handleKeyReleaseDirection();
-    	}
-    }
-    
-    private void handleKeyReleaseDirection() {
+    private void handleDirection() {
     	ArrayList<Direction> directionsPressed = getDirectionsPressed();
     	if (directionsPressed.size() == 1 && directionsPressed.get(0) != getPlayer().getDirection()) {
     		setDirection(directionsPressed.get(0));
-    	} else if (getPlayer().getDirection() != Direction.NONE) {
+    	} else if (directionsPressed.size() == 0 || directionsPressed.size() > 1) {
     		setDirection(Direction.NONE);
     	}
     }
