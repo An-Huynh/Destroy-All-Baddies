b1fe72a69269f51ce7ea63d390129c6f6d002c45
Pugglesworth <rekilad@live.com>
2017-04-21 21:27:41 -0500

    Reformated the Java Launch Window

--- src/dab/DestroyAllBaddies.java
+++ src/dab/DestroyAllBaddies.java
@@ -4,7 +4,6 @@ import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
 import dab.client.GameClient;
 import dab.server.GameServer;
 
@@ -13,6 +12,8 @@ import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -26,34 +27,51 @@ implements ActionListener {
 	protected static JFrame frame;
 	
 	public DestroyAllBaddies() {
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
 		// Welcome Label Settings
+		c.gridy = 0;
+		c.gridwidth = 2;
+		c.insets = new Insets(10,0,0,0);
 		welcomeLabel = new JLabel();
 		welcomeLabel.setText("Welcome to...");
+		add(welcomeLabel, c);
 		
 		// DAB Label Settings
+		c.gridy = 1;
 		dabLabel = new JLabel();
 		dabLabel.setText("Destroy All Baddies!");
+		add(dabLabel, c);
 		
 		// Host Icon
 		ImageIcon hostIcon = createImageIcon("/resource/img/icon/host.png");
 		
 		// Host Button Settings
+		c.gridy = 2;
+		c.gridwidth = 1;
+		c.insets = new Insets(10,0,10,0);
 		hostButton = new JButton("Host", hostIcon);
         hostButton.setVerticalTextPosition(AbstractButton.BOTTOM);
         hostButton.setHorizontalTextPosition(AbstractButton.CENTER);
         hostButton.setMnemonic(KeyEvent.VK_H);
         hostButton.setActionCommand("host");
         hostButton.setFocusPainted(false);
+        add(hostButton, c);
         
         // Join Icon
         ImageIcon joinIcon = createImageIcon("/resource/img/icon/join.png");
         
         // Join Button Settings
+        c.gridy = 2;
+        c.gridx = 1;
+        c.insets = new Insets(0,10,0,0);
         joinButton = new JButton("Join", joinIcon);
         joinButton.setVerticalTextPosition(AbstractButton.BOTTOM);
         joinButton.setHorizontalTextPosition(AbstractButton.CENTER);
         joinButton.setMnemonic(KeyEvent.VK_J);
         joinButton.setActionCommand("join");
+        add(joinButton, c);
         
         // Play Icon
         ImageIcon playIcon = createImageIcon("/resource/img/icon/play.png");
@@ -61,7 +79,49 @@ implements ActionListener {
         // Play Filled Icon
         ImageIcon playFilledIcon = createImageIcon("/resource/img/icon/playFilled.png");
         
+        // IP Address Label Settings
+        c.gridy = 0;
+        c.gridx = 1;
+        c.gridwidth = 2;
+        c.insets = new Insets(5,5,5,5);
+        IPAddressLabel = new JLabel();
+        IPAddressLabel.setText("IP Address:");
+        IPAddressLabel.setVisible(false);
+        add(IPAddressLabel, c);
+        
+        // IP Address Field
+        c.gridy = 1;
+        c.gridx = 0;
+        c.gridwidth = 3;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        IPAddressField = new JTextField(20);
+        IPAddressField.setVisible(false);
+        add(IPAddressField, c);
+        
+        // Character Name Label Settings
+        c.gridy = 2;
+        c.gridx = 1;
+        c.gridwidth = 2;
+        c.fill = GridBagConstraints.NONE;
+        nameLabel = new JLabel();
+        nameLabel.setText("Character Name:");
+        nameLabel.setVisible(false);
+        add(nameLabel, c);
+        
+        // Character Name Field
+        c.gridy = 3;
+        c.gridwidth = 3;
+        c.gridx = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        nameField = new JTextField(20);
+        nameField.setVisible(false);
+        add(nameField, c);
+        
         // Launch Client and Server Button Settings
+        c.gridy = 4;
+        c.gridwidth = 2;
+        c.gridx = 1;
+        c.fill = GridBagConstraints.NONE;
         launchBothButton = new JButton("Launch", playIcon);
         launchBothButton.setVerticalAlignment(AbstractButton.BOTTOM);
         launchBothButton.setVerticalTextPosition(AbstractButton.BOTTOM);
@@ -71,8 +131,10 @@ implements ActionListener {
         launchBothButton.setActionCommand("launchBoth");
         launchBothButton.setRolloverIcon(playFilledIcon);
         launchBothButton.setVisible(false);
+        add(launchBothButton, c);
         
         // Launch Client Button Settings
+        c.gridy = 4;
         launchClientButton = new JButton("Launch", playIcon);
         launchClientButton.setVerticalAlignment(AbstractButton.BOTTOM);
         launchClientButton.setVerticalTextPosition(AbstractButton.BOTTOM);
@@ -82,7 +144,8 @@ implements ActionListener {
         launchClientButton.setActionCommand("launchClient");
         launchClientButton.setRolloverIcon(playFilledIcon);
         launchClientButton.setVisible(false);
-  
+        add(launchClientButton, c);
+        
         // Listen for actions on the buttons
         hostButton.addActionListener(this);
         launchBothButton.addActionListener(this);
@@ -94,41 +157,9 @@ implements ActionListener {
         launchBothButton.setToolTipText("Click this button to launch the server and game!");
         joinButton.setToolTipText("Click this button to join a hosted game.");
         launchClientButton.setToolTipText("Click this button to join the game!");
-
-        // IP Address Label Settings
-        IPAddressLabel = new JLabel();
-        IPAddressLabel.setText("IP Address:");
-        IPAddressLabel.setVisible(false);
         
-        // IP Address Field
-        IPAddressField = new JTextField(20);
-        IPAddressField.setVisible(false);
+        // Add everything to the view 
         
-        // Character Name Label Settings
-        nameLabel = new JLabel();
-        nameLabel.setText("Character Name:");
-        nameLabel.setVisible(false);
-        
-        // Character Name Field
-        nameField = new JTextField(20);
-        nameField.setVisible(false);
-        
-        // Constraints for the two fields to take up more space
-        GridBagConstraints c = new GridBagConstraints();
-        c.gridwidth = GridBagConstraints.REMAINDER;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        
-        // Add everything to the view
-        add(IPAddressLabel);
-        add(IPAddressField, c);
-        add(nameLabel);
-        add(nameField, c);
-        add(welcomeLabel);
-        add(dabLabel);
-        add(hostButton);
-        add(launchBothButton);
-        add(joinButton);
-        add(launchClientButton);
 	}
 	
 	/**
@@ -157,7 +188,7 @@ implements ActionListener {
             nameLabel.setVisible(true);
             nameField.setVisible(true);
             launchClientButton.setVisible(true);
-            frame.setSize(300, 230);
+            frame.setSize(300, 270);
         } else if("host".equals(e.getActionCommand())) {
         	// Clear the screen and remove unused buttons
             joinButton.setVisible(false);
@@ -172,7 +203,7 @@ implements ActionListener {
             nameLabel.setVisible(true);
             nameField.setVisible(true);
             launchBothButton.setVisible(true);
-            frame.setSize(300, 180);
+            frame.setSize(300, 210);
         } else if("launchBoth".equals(e.getActionCommand())) {
         	// Use Threads to Run the Server and Client simultaneously. Then wait for client, and the server to end.
         	try {
@@ -251,11 +282,11 @@ implements ActionListener {
         frame.setContentPane(newContentPane);
 
         //Frame Icon
-        ImageIcon icon = createImageIcon("/resource/img/player/player.png");
+        ImageIcon icon = createImageIcon("/resource/img/player/player_green.png");
         
         //Display the window.
         frame.setIconImage(icon.getImage());
-        frame.setSize(300, 155);
+        frame.setSize(300, 230);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
