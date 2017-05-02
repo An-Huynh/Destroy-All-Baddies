518772a7d514cc7bcf8ec8af7f31ec84d7c0045e
An Huynh <anthehuynh@gmail.com>
2017-05-01 21:06:50 -0500

    Add HomeZone

---
+++ src/main/java/dab/server/world/zone/HomeZone.java
@@ -0,0 +1,34 @@
+package dab.server.world.zone;
+
+import dab.server.Server;
+import dab.server.world.tile.TileLocation;
+
+public class HomeZone extends Zone
+{
+	public HomeZone()
+	{
+		super();
+		setID("dab:zone:home");
+		
+		// add top and bottom
+		for (int x = 0; x < 12; ++x)
+		{
+			addTile(Server.getTileRegistry().get("dab:tile:solid"), x, 0);
+			addTile(Server.getTileRegistry().get("dab:tile:solid"), x, 12);
+		}
+		
+		// add left wall
+		for (int y = 0; y < 9; ++y)
+		{
+			addTile(Server.getTileRegistry().get("dab:tile:solid"), -1, y);
+		}
+		
+		// add right side teleporters
+		TileLocation rightSideDest = new TileLocation("dab:zone:start", 0, 1);
+		for (int y = 0; y < 9; ++y)
+		{
+			addTile(Server.getTileRegistry().get("dab:tile:teleporter"), 12, y);
+			addTeleporter(new TileLocation("dab:zone:home", 12, y), rightSideDest);
+		}
+	}
+}
