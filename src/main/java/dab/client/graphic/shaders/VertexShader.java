90c3bc18cda9326f5094e67b4a556fd1d8493571
An Huynh <anthehuynh@gmail.com>
2017-05-01 20:54:18 -0500

    Add VertexShader

---
+++ src/main/java/dab/client/graphic/shaders/VertexShader.java
@@ -0,0 +1,51 @@
+package dab.client.graphic.shaders;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+
+public class VertexShader implements Shader
+{
+	private int shader;
+	
+	@Override
+	public void create()
+	{
+		shader = GL20.glCreateShader(GL20.GL_VERTEX_SHADER);
+	}
+	
+	@Override
+	public void loadSource(String sourceCode)
+	{
+		GL20.glShaderSource(shader, sourceCode);
+	}
+	
+	@Override
+	public void compile()
+	{
+		GL20.glCompileShader(shader);
+	}
+	
+	@Override
+	public boolean compileResult()
+	{
+		return GL20.glGetShaderi(shader, GL20.GL_COMPILE_STATUS) == GL11.GL_TRUE;
+	}
+	
+	@Override
+	public String getLog()
+	{
+		return GL20.glGetShaderInfoLog(shader);
+	}
+	
+	@Override
+	public int getShaderID()
+	{
+		return shader;
+	}
+	
+	@Override
+	public void delete()
+	{
+		GL20.glDeleteShader(shader);
+	}
+}
